baf08f3eb30fd08b0478fa4de9bcc95567fa0c27
Listyle1 <popov.grihon2013@mail.ru>
2016-03-17 19:38:24 +0300

    MyCorteg

--- MyArray/Program.cs
+++ MyArray/Program.cs
@@ -1,45 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace MyArray
 {
-}
-class Program
-{
-    public string Array(int[] numbers)//Метод Array принимает массив int[] numbers
+    class Program
     {
-        int max = int.MinValue;
-        int min = int.MaxValue;
-        int summ = 0;
-        for (int i = 0; i < numbers.Length; i++)
+        public Tuple<int, int, int> Corteg (int[] numbers)
         {
-            summ += numbers[i];//сумма элементов массива
-            Console.Write(" {0},", numbers[i]);
-            if (max < numbers[i])
+            int max = int.MinValue;
+            int min = int.MaxValue;
+            int summ = 0;
+            for (int i = 0; i < numbers.Length; i++)
             {
-                max = numbers[i];
+                summ += numbers[i];//сумма элементов массива
+                Console.Write(" {0},", numbers[i]);
+                if (max < numbers[i])
+                {
+                    max = numbers[i];
+                }
+                else if (min > numbers[i])
+                {
+                    min = numbers[i];
+                }
             }
-            if (min > numbers[i])
-            {
-                min = numbers[i];
-            }
+            return Tuple.Create<int, int, int>(max, min, summ / numbers.Length);
         }
-        return " \nМаксимальное значение: " + max + " \nМинимальное значение: " + min + " \nСреднее значение: " + summ/numbers.Length;
-    }
-    static void Main(string[] args)
-    {
-        int[] numbers = new int[100];
-        Random r = new Random();//рандомное значение переменной r
-        for (int i = 0; i < numbers.Length; i++)//присваивает каждому эл-ту массива рандомное число от 50 до 100
+        static void Main(string[] args)
         {
-            numbers[i] = r.Next(50, 100);
+            int[] numbers = new int[100];
+            Random r = new Random();//рандомное значение переменной r
+            for (int i = 0; i < numbers.Length; i++)//присваивает каждому эл-ту массива рандомное число от 50 до 100
+            {
+                numbers[i] = r.Next(50, 100);
+            }
+            Program Arg = new Program();//Arg экземпляр класса Program
+            Console.WriteLine(Arg.Corteg(numbers));//вызов метода и передача ему массива numbers
+            Console.ReadKey();
         }
-        Program Arg = new Program();//Arg экземпляр класса Program
-        Console.WriteLine(Arg.Array(numbers));//вызов метода и передача ему массива numbers
-        Console.ReadKey();
     }
 }
 
